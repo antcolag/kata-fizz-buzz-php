bbce90fd0b59adb428d8d42da1216e7e77f803fe
anto <antcolag@gmail.com>
2022-07-24 01:24:21 +0200

    feat: introduce FizzBuzz class

--- src/Kata/FizzBuzz.php
+++ src/Kata/FizzBuzz.php
@@ -4,8 +4,27 @@ namespace Kata;
 
 class FizzBuzz
 {
-    public function handle(): bool
+    public Printer $printer;
+
+    public function __construct(Printer $printer = null) {
+        $this->printer = $printer;
+    }
+
+    public function handle(int $arg = 100): bool
     {
+        $current = 0;
+        while(++$current <= $arg){
+            if(!($current % 15)) {
+                $this->printer->print('FizzBuzz');
+            } elseif(!($current % 3)) {
+                $this->printer->print('Fizz');
+            } elseif(!($current % 5)) {
+                $this->printer->print('Buzz');
+            } else {
+                $this->printer->print((string) $current);
+            }
+        }
+
         return true;
     }
 }
